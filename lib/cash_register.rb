db9176b4ff0c1c0cb5050137ac4e865f95f80e1c
Nhinh Dao <nhinhdao.vn@gmail.com>
2018-06-27 17:53:56 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,28 @@
+class CashRegister
+    attr_accessor :total
+    @@total = 0
+    def initialize(total)
+        @total = total
+    end
 
+    def total
+        @@total
+    end
+
+    def add_item(title, price)
+
+
+    end
+
+    def apply_discount
+
+    end
+
+    def items
+
+    end
+
+    def void_last_transaction
+
+    end
+end
