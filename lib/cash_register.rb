cc28c6b757c4ab412227e0443367dc6c5ec3fe5b
Nhinh Dao <nhinhdao.vn@gmail.com>
2018-06-27 18:00:12 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,12 +1,12 @@
 class CashRegister
     attr_accessor :total
-    # @@total = 0
+    @@total = 0
     def initialize(total)
         total == nil ? @total = 0 : @total = total
     end
 
     def total
-        @total
+        @@total
     end
 
     def add_item(title, price)
