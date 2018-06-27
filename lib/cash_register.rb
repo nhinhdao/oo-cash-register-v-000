adf74dd197c97c4beba9b248f4f76db2e30d15df
Nhinh Dao <nhinhdao.vn@gmail.com>
2018-06-27 19:05:16 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -12,7 +12,7 @@ class CashRegister
 
     def add_item(item, price, num = 1)
         @total += price*num
-        (@total*@discount(1-@discount.to_f/100)).to_i
+        @total = (@total*@discount(1-@discount.to_f/100)).to_i
     end
 
     def apply_discount
