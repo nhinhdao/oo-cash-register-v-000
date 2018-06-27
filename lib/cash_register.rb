77884df83920a06d18b4c03933de59860426ddb7
Nhinh Dao <nhinhdao.vn@gmail.com>
2018-06-27 19:31:43 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,3 +1,4 @@
+require'pry'
 class CashRegister
     attr_accessor :total, :discount
 
@@ -15,6 +16,7 @@ class CashRegister
     end
 
     def apply_discount
+        binding.pry
         @total = (@total*(1-@discount/100.0)).to_i
         "After the discount, the total comes to $#{@total}."
     end
